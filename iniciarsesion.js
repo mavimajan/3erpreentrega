5be5f1e6062bfc2dd4e3609c9de369eca732abb8
maria majan <mavi.majan@gmail.com>
2023-02-26 22:41:10 -0300

    cambios en iniciarsesionjs

--- iniciarsesion.js
+++ iniciarsesion.js
@@ -1,8 +1,11 @@
+// capturar datos ingresados en formulario al hacer click en iniciar sesion. A partir de la captura confirmar si es o no usuario. De serlo podra realizar la compra, sino debe registrarse. Para registrarse se abre un formulario flotante, la condicion es que el usuario sea mayor de edad. 
 
+
+// ** se crea la funcion capturar datos para usarla con el addeventlistener click
 
 function capturar_datos(){
-    console.log(localStorage.setItem("email", inputEmail));
-    console.log(localStorage.setItem("password", inputPassword));
+    localStorage.setItem("email", inputEmail);
+    localStorage.setItem("password", inputPassword);
 }
 
 let btn_ingresar = document.getElementById("btn_ingresar");
